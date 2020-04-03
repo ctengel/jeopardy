280c114cd88d865e825ca37a565c77a8db084a86
Chris Engelhart <engleman@gmail.com>
2020-04-03 19:11:19 -0400

    Refactoring js and adding swagger

---
+++ README.md
@@ -0,0 +1,11 @@
+Trivia game
+
+* static has html
+* swagger.yaml has Swagger 2.0 API spec
+* Serverless API implementation in Python forthcoming
+
+To run, place the static files in a web server, put URL of API in config.js, and navigate to alex.html
+
+TODO
+* Add API
+* Lock username
---
+++ static/common.js
@@ -0,0 +1,13 @@
+
+function time2read(timemili) {
+	var disptime = new Date(timemili);
+	return disptime.getHours().toString().padStart(2, '0') + ':' + disptime.getMinutes().toString().padStart(2, '0') + ':' + disptime.getSeconds().toString().padStart(2, '0') + '.' + disptime.getMilliseconds().toString().padStart(3, '0');
+}
+
+function processbuzz(x) {
+	var node = document.createElement('li');
+	var textnode = document.createTextNode(time2read(x['datetime'] + ' - ' + x['person']);
+	node.appendChild(textnode);
+	document.getElementById('buzzes').appendChild(node);
+	return x['datetime'];
+}
