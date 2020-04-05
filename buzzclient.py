f15be6a93cdfd6c446f4cc2a0ac8cb9c277e6e03
Chris Engelhart <engleman@gmail.com>
2020-04-05 15:32:57 -0400

    Bugfix

--- buzzclient.py
+++ buzzclient.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 import sys
 
-r = requests.post('https://6fyvhv2ao3.execute-api.us-east-1.amazonaws.com/dev1/buzz', json={'person':sys.argv[0]})
+r = requests.post('https://6fyvhv2ao3.execute-api.us-east-1.amazonaws.com/dev1/buzz', json={'person':sys.argv[1]})
 print(r.json())
